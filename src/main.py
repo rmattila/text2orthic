a97893bfe43d6289110237f629c6120bb8105c99
Robert <robfrog@gmail.com>
2024-01-21 12:32:00 +0100

    change to hello world message

--- src/main.py
+++ src/main.py
@@ -3,15 +3,12 @@ from glyph_renderer import GlyphRenderer
 
 
 def main():
-    word = "pRoofoprop"
+    word = "abaicdefghijklmnopqurstuvwxyzhelloworld"
 
     encoder = OrthicEncoder()
     renderer = GlyphRenderer()
 
     glyphs = encoder.encode_word(word)
-    for glyph in glyphs:
-        print(glyph)
-
     rendered_image = renderer.render_word(glyphs)
 
     rendered_image.show()
